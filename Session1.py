70ebc5fad7f05ae223f2d9ea3758c6dc10f5db3f
Your Name <you@example.com>
2026-05-29 03:53:44 +0100

    i Session 1 homework

---
+++ Session1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+city = input("where do you live? ")
+favorite_subject = input("what is your favorite subject? ")
+
+print(f'Hello! My name is {name}, I live in {city}, and my favorite school subject is {favorite_subject}.')
