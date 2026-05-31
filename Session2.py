e586bea9803f2a597b9ed08e629aaa05c7db5c2f
Your Name <you@example.com>
2026-05-31 13:51:54 +0100

    Session 2 improved

--- Session2.py
+++ Session2.py
@@ -1,9 +1,9 @@
-name = input(f'What is your name? ')
-mark1 = float(input(f"What is your mark in English? "))
-mark2 = float(input(f"What is your mark in French? "))
-mark3 = float(input(f"What is your mark in Philosophy? "))
+name = input('What is your name? ')
+mark1 = float(input("What is your mark in English? "))
+mark2 = float(input("What is your mark in French? "))
+mark3 = float(input("What is your mark in Philosophy? "))
 
-average = (mark1 + mark2 + mark3) / 3
+average = round((mark1 + mark2 + mark3) / 3, 2)
 passed = average >= 10
 print(f"Hello {name}! Your average in English, French, and Philosophy is {average}.")
-print(f"Passed: {passed}")
+print(f"Passed: {'Yes' if passed else 'No'}")
